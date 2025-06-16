2b24f755d0366598bbfde2e4cf404846d28b0d9e
Cisco Osterman <cisco@twistedsolutions.se>
2025-06-16 11:18:50 +0200

    Refactor LoggingMiddleware to include response duration and status code logging

--- middleware/logger.go
+++ middleware/logger.go
@@ -2,23 +2,44 @@ package middleware
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
 // LoggingMiddleware logs each incoming HTTP request in JSON format.
 func LoggingMiddleware(next http.Handler) http.Handler {
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		lrw := &loggingResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
 		logrus.WithFields(logrus.Fields{
 			// ECS field names
 			"http.request.method": r.Method,
 			"url.original":        r.RequestURI,
+			"url.path":            r.URL.Path,
 			"source.ip":           r.RemoteAddr,
 			"event.dataset":       "http.request",
 			"event.kind":          "event",
 		}).Info("incoming HTTP request")
 
-		next.ServeHTTP(w, r)
+		start := time.Now()
+		next.ServeHTTP(lrw, r)
+		elapsed := time.Since(start).Milliseconds()
+
+		logrus.WithFields(logrus.Fields{
+			"http.response.status_code": lrw.statusCode,
+			"event.dataset":             "http.response",
+			"event.kind":                "event",
+			"event.duration":            elapsed,
+		}).Info("HTTP response sent")
 	})
 }
